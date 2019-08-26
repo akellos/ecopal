98b1d55957e8dafdceb5819a892727db158c51bd
Evia Horoneko <evija.horoneko@gmail.com>
2019-08-26 17:18:58 +0100

    Added the routes and respective method in controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :challenges do
+    resources :trackers, only: [:create, :destroy, :update]
+  end
+
+  get 'dashboard', to: 'pages#dashboard'
+
+  root to: 'pages#home'
 end
