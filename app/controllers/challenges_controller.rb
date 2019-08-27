127331cc3a1acf70ca5531fe80ec4413aff17c2b
Evia Horoneko <evija.horoneko@gmail.com>
2019-08-27 12:14:20 +0100

    Created challenges controller and respective views

---
+++ app/controllers/challenges_controller.rb
@@ -0,0 +1,40 @@
+class ChallengesController < ApplicationController
+
+  def show
+    @challenge = Challenge.find(params[:id])
+  end
+
+  def index
+    @challenges = Challenge.all
+  end
+
+  def new
+    @challenge = Challenge.new
+  end
+
+  def create
+    @challenge = Challenge.new(challenge_params)
+    if @challenge.save
+      redirect_to challenge_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @challenge = Challenge.find(params[:id])
+    @challenge.update(params[:id])
+  end
+
+  def destroy
+    @challenge = Challenge.find(params[:id])
+    @challenge.destroy
+    redirect_to challenge_path
+  end
+
+  private
+
+  def challenge_params
+    params.require(:challenge).permit(:name, :duration, :start_date, :end_date, :description, :image, :reward, :address)
+  end
+end
