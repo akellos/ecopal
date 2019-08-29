5d79b64130f1dabcf6d65316e44b942a6cb07746
Ahmad Majid <Ahmadmajid1994@gmail.com>
2019-08-29 18:05:06 +0100

    finished friends, you can add a friend, but now I need to display the friends in a friends list

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,15 +1,13 @@
 class Friendship < ApplicationRecord
   belongs_to :sender, :class_name => 'User'
   belongs_to :recipient, :class_name => 'User'
-  belongs_to :user, foreign_key: true
-  belongs_to :friend, class_name: "User" #this line says you'll have another foreign key in here called friend_id, but it's actually a member of the model User
+  # belongs_to :user, foreign_key: true
+  # belongs_to :friend, class_name: "User" #this line says you'll have another foreign key in here called friend_id, but it's actually a member of the model User
 
   def self.create_reciprocal_for_ids(user_id, friend_id)
-    user_friendship = Friendship.create(user_id: user_id, friend_id: friend_id)
+    user_friendship = Friendship.create(sender: User.find(user_id), recipient: User.find(friend_id), status: "pending")
 
-    friend_friendship = Friendship.create(user_id: friend_id, friend_id: user_id)
-
-    [user_friendship, friend_friendship]
+    friend_friendship = Friendship.create(sender: User.find(friend_id), recipient: User.find(user_id), status: "pending")
   end
 
   def self.destroy_recriprocal_for_ids(user_id, friend_id)
