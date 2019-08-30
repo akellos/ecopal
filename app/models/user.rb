c870d52fa263186afb91b75c359093f988e8c454
Ahmad Majid <Ahmadmajid1994@gmail.com>
2019-08-30 12:12:48 +0100

    friends list now showing friends, only issue is that you can have duplicate friends, but I'm going to wrtie some more code to fix this

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,14 @@ class User < ApplicationRecord
   has_many :trackers
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
-<<<<<<< HEAD
   # has_many :sent_friendships, :class_name => 'Friendship', :foreign_key => 'sender_id'
   has_many :recipient, :class_name => 'Friendship', :foreign_key => 'recipient_id'
   has_many :friendships, dependent: :destroy
   # has_many :friends, through: :friendships
-=======
 
   mount_uploader :image, PhotoUploader
->>>>>>> 02388036f477c0c0ae1060aae3e668eaff09aa18
+
+  def list_friends
+    Friendship.where(recipient: self)
+  end
 end
