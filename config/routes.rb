70db41ced46f1bcd7f6cdc5c07a6115f6a34975d
Moritz Indenhuck <indenhuck@posteo.de>
2019-09-02 17:41:39 +0100

    Added Tracker update

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :challenges do
-    resources :trackers, only: [:create, :destroy, :update]
+    resources :trackers, only: [:create, :destroy, :update, :show]
   end
 
   get 'dashboard', to: 'pages#dashboard'
 
+  patch "trackers/:id/update_tracker_days", to: "trackers#update_tracker_days", as: :update_tracker_days
 
   root to: 'pages#home'
 
