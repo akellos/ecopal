6556d865d41d613dbc207002e1cd4c7b367c52ed
Lila Liu <lilayliu@gmail.com>
2019-09-03 15:03:52 +0100

    added duration to reward method and random colors on challenge creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_02_155919) do
+ActiveRecord::Schema.define(version: 2019_09_03_135529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_09_02_155919) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.string "photo"
+    t.string "bg_color"
     t.index ["user_id"], name: "index_challenges_on_user_id"
   end
 
