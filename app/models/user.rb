c70d180398568c361853338d3ac7d41653e033bc
Ahmad Majid <Ahmadmajid1994@gmail.com>
2019-09-03 09:36:27 +0100

    fixing friends, this first commit is just so I can pull

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ApplicationRecord
+  class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_location?
   # has_many :sent_friendships, :class_name => 'Friendship', :foreign_key => 'sender_id'
   has_many :recipient, :class_name => 'Friendship', :foreign_key => 'recipient_id'
-  has_many :friendships, dependent: :destroy
+  # has_many :friendships, dependent: :destroy
   # has_many :friends, through: :friendships
 
   mount_uploader :photo, PhotoUploader
