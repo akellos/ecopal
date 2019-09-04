0076c11c0efc108a23c93df7e491fc7acbb66cfa
Ahmad Majid <Ahmadmajid1994@gmail.com>
2019-09-04 14:36:02 +0100

    removed opacity, moved cards up, fixed pal because these changes effected him

--- app/javascript/packs/pal.js
+++ app/javascript/packs/pal.js
@@ -1,10 +1,15 @@
 window.addEventListener("scroll", (event) => {
   var scrollTop = $(window).scrollTop() ;
+  const pal = document.querySelector(".pal-landing");
+
   if (scrollTop > 705) {
-    const pal = document.querySelector(".pal-landing");
     pal.style.position = "absolute";
     pal.style.top = "36%";
+  } else {
+    pal.style.position = "fixed";
+    pal.style.top = "150px";
   }
+
   if (scrollTop > 715) {
     const bubble = document.querySelector(".bubble");
     bubble.style.position = "absolute";
